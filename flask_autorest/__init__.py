e14049b87a3ba4303a20539fbd137931a9e5b643
dantezhu <dantezhu@qq.com>
2014-08-08 21:29:27 +0800

    先提交

--- flask_autorest/__init__.py
+++ flask_autorest/__init__.py
@@ -1,3 +1,49 @@
 # -*- coding: utf-8 -*-
 
 __version__ = '0.1.1'
+
+from flask.views import MethodView
+from flask import Blueprint
+
+AUTOREST_BLUEPRINT_NAME = 'autorest'
+AUTOREST_URL_PREFIX = '/autorest'
+
+
+class AutoRest(object):
+
+    def __init__(self, app=None):
+        """init with app
+
+        :app: Flask instance
+
+        """
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """
+        安装到app上
+        """
+
+    def create_blueprint(self, app, sources):
+        """
+        生成一个blueprint
+        """
+        blueprint_name = app.config.get('AUTOREST_BLUEPRINT_NAME') or AUTOREST_BLUEPRINT_NAME
+        url_prefix = app.config.get('AUTOREST_URL_PREFIX') or AUTOREST_URL_PREFIX
+        sources = app.config.get('AUTOREST_SOURCES')
+
+        bp = Blueprint(blueprint_name, __name__, url_prefix=url_prefix)
+
+
+class ResourceView(MethodView):
+    """
+    /resource/<id>
+    """
+    pass
+
+
+class ResourceListView(MethodView):
+    """
+    /resource/
+    """
